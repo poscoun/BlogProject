006ef35b0e32040a182bb598a36f40431014e80e
JYJ <poscoun@gmail.com>
2021-05-13 18:17:50 +0900

    2021-05-13

---
+++ BlogProject/src/main/java/com/gblog/dto/categoryDTO.java
@@ -0,0 +1,12 @@
+package com.gblog.dto;
+
+import lombok.Data;
+
+@Data
+public class categoryDTO {
+	private int category_id;
+	private int post_id;
+	private String category_name;
+	private int post_count;
+	
+}
