c8ff1fedf99821e8a680038d1fe96d817fa11e03
kimjy2589 <jiyoung2589@gmail.com>
2021-05-13 17:08:15 +0900

    dto

---
+++ BlogProject/src/main/java/com/gblog/dto/ReplyDTO.java
@@ -0,0 +1,61 @@
+package com.gblog.dto;
+
+import java.util.Date;
+
+public class ReplyDTO {
+	public int rp_id;
+	public String rp_writer;
+	public String rp_content;
+	public Date rp_reg;
+	public String post_id;
+	
+	public ReplyDTO(int rp_id, String rp_writer, String rp_content, Date rp_reg, String post_id) {
+		super();
+		this.rp_id = rp_id;
+		this.rp_writer = rp_writer;
+		this.rp_content = rp_content;
+		this.rp_reg = rp_reg;
+		this.post_id = post_id;
+	}
+
+	public int getRp_id() {
+		return rp_id;
+	}
+
+	public void setRp_id(int rp_id) {
+		this.rp_id = rp_id;
+	}
+
+	public String getRp_writer() {
+		return rp_writer;
+	}
+
+	public void setRp_writer(String rp_writer) {
+		this.rp_writer = rp_writer;
+	}
+
+	public String getRp_content() {
+		return rp_content;
+	}
+
+	public void setRp_content(String rp_content) {
+		this.rp_content = rp_content;
+	}
+
+	public Date getRp_reg() {
+		return rp_reg;
+	}
+
+	public void setRp_reg(Date rp_reg) {
+		this.rp_reg = rp_reg;
+	}
+
+	public String getPost_id() {
+		return post_id;
+	}
+
+	public void setPost_id(String post_id) {
+		this.post_id = post_id;
+	}
+		
+}
